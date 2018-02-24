b4e867d4c78b97a0165db3b1b5f20e75ee85fa6f
pocandraspoc <pocandraspoc@gmail.com>
2018-02-24 01:04:55 +0100

    Print NoneType

--- AAEStrings.py
+++ AAEStrings.py
@@ -6,6 +6,36 @@ def clean(text):
 		return ret
 	except InvalidOperation:
 		return text
-
-obj = clean_decimal("hello, my name is $ Joe, what do you think")
+'''
+obj = clean("hello, my name is $ Joe, what do you think")
 print(obj)
+'''
+
+
+'''
+Because of The None type in clean i tried something out
+obj = None
+print(type(print(obj)))
+
+What Gave an interesting result:
+None
+<class 'NoneType'>
+
+SO
+the inner print WAS evaluated,
+THEN
+the outer print got evaluated!
+end type of print(None) IS NoneType 
+
+Well lets take a look on 
+obj = type(print("a"))
+print(obj)
+
+a
+<class 'NoneType'>
+
+WooooW,
+So type(print(f(x)))
+IS NoneType!
+'''
+
