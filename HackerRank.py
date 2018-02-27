344a324834329527c2ff5b654a168faf2ccbb60e
pocandraspoc <pocandraspoc@gmail.com>
2018-02-27 23:58:28 +0100

    checking some type

---
+++ HackerRank.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+def simpleArraySum(n, ar):
+	preSum = 0
+    for index in range(n):
+    	preSum += ar[index]
+    return preSum
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = simpleArraySum(n, ar)
+print(result)
